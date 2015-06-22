3744a4e6444aa65e307e8d15e478b2a6374ccce3
Scubaboy <robin@robincawsey.plus.com>
2015-06-22 20:26:49 +0100

    added missing interface

---
+++ QuadComms/Interfaces/SignalR/ISignalRClientProxy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadComms.Interfaces.SignalR
+{
+    public interface ISignalRClientProxy
+    {
+        List<Type> SupportedMsgTypes { get; }
+
+        Task<bool> Post<T>(T msg);
+
+        Task StartClientProxy();
+
+    }
+}
